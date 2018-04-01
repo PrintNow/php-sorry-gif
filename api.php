4dfb050a0730f4b27e8a9112a5b359382709e589
NowTime <chenwenzhou@aliyun.com>
2018-04-01 22:01:00 +0800

    Update api.php

--- api.php
+++ api.php
@@ -20,13 +20,13 @@ if($type && $data && $small){
     $change_ass = str_replace($str_source,$data,$ass_file);
     file_put_contents($CACHE_ASS_PATH,$change_ass);
 
-    $out_put_file = ROOT.'cache/'.$request_time.'.gif';
-    $command = 'ffmpeg -i '.$TEMP_VIDEO.' -s 236*132 -vf "ass='.$CACHE_ASS_PATH.'" '.$out_put_file;
-    system($command);
+    $out_put_file = ROOT.'/cache/'.$request_time.'.gif';
+    $command = 'ffmpeg -y -i '.$TEMP_VIDEO.' -vf "ass='.$CACHE_ASS_PATH.'" '.$out_put_file;
+    system($command,$input);
 
     $result['code'] = 200;
     $result['msg'] = '应该生成成功...';
-    $result['path'] = $out_put_file;
+    $result['path'] = '/cache/'.$request_time.'.gif';
   }else{
     $result['code'] = 404;
     $result['msg'] = '模板文件不存在！';
@@ -37,4 +37,4 @@ if($type && $data && $small){
   $result['msg'] = '缺少必要参数，请检查！';
 }
 
-print_r($result);
+echo json_encode($result);
